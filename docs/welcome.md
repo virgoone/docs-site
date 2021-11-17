28f3eb355c0e6e556fa4e481895837f6e348eda6
virgoone <me@virgo.one>
2021-11-17 20:11:31 +0800

    初始化

---
+++ docs/welcome.md
@@ -0,0 +1,20 @@
+---
+sidebar_position: 1
+---
+
+# 入门
+
+**Lark**常用工具介绍，是一个快速创建 React 单页应用的方法和服务，提供了零配置的构建设置
+
+## Lark CLI
+
+与 Create React App 功能类似，提供 快速创建项目功能
+
+具体文档可查看 [`lark-cli`](/docs/lark-cli)
+
+## Lark CLI Service
+
+主要功能参考 react-scripts，做了一些特殊封装，可快速创建项目、构建项目，基本零配置
+
+具体文档可查看 [`lark-cli-service`](/docs/lark-cli-service)
+
---
+++ docusaurus.config.js
@@ -0,0 +1,76 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/duotoneLight');
+const darkCodeTheme = require('prism-react-renderer/themes/duotoneDark');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Lark',
+  tagline: 'Lark Tools are cool',
+  url: 'https://lark-tools.netifly.app',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'lark-org', // Usually your GitHub org/user name.
+  projectName: 'docs-site', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Lark Tools Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'welcome',
+            position: 'right',
+            label: '文档',
+          },
+          {
+            href: 'https://github.com/lark-org',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Lark, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
